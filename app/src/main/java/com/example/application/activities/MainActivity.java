db71cc59b29f083ca100d601fdc1f6a2b1bc1b84
Phyrocycle27 <45393999+Phyrocycle27@users.noreply.github.com>
2020-09-17 21:59:03 +0300

    area_item.xml was added

--- app/src/main/java/com/example/application/activities/MainActivity.java
+++ app/src/main/java/com/example/application/activities/MainActivity.java
@@ -1,20 +1,12 @@
 package com.example.application.activities;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.application.R;
 import com.example.application.activities.devices.creation.DeviceCreationFragment;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,58 +20,5 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_add) {
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction ft = manager.beginTransaction();
-
-            Fragment deviceCreationFrag = manager.findFragmentByTag(DEVICE_CREATION);
-
-            if (deviceCreationFrag == null) {
-                ft.add(R.id.fragment_container, new DeviceCreationFragment(), DEVICE_CREATION)
-                        .hide(Objects.requireNonNull(manager.findFragmentById(R.id.devices_list_fragment)))
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .addToBackStack(DEVICE_CREATION)
-                        .commit();
-
-                setToolbarTitle("Создание устройства");
-            } else {
-                goToBack();
-            }
-        }
-        return true;
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        int count = getSupportFragmentManager().getBackStackEntryCount();
-
-        if (count == 0) {
-            super.onBackPressed();
-        } else {
-            goToBack();
-        }
-    }
-
-    private void goToBack() {
-        getSupportFragmentManager().popBackStack();
-
-        Fragment f = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-
-        Log.d(TAG, "f instance is " + (f instanceof DeviceCreationFragment) + " f visible is " + Objects.requireNonNull(f).isVisible());
-    }
-
-    public void setToolbarTitle(String title) {
-        Objects.requireNonNull(getSupportActionBar()).setTitle(title);
     }
 }
